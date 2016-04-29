45891e3483510e38a66193e983c8842395704875
Mantas Kaveckas <kaveckasm@gmail.com>
2016-04-29 20:33:22 +0300

    add webpack provider plugin

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
+  ]
+};
